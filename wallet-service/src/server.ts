2dbdff323bc36c0334a9f20eef3fb4e6607f3272
tarcisio-melo-sp <tarcisio.melo@shawandpartners.com>
2023-12-11 21:06:48 -0300

    feat: setup wallet-service

---
+++ wallet-service/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { env } from "./core/env";
+
+dotenv.config();
+
+const app: Express = express();
+const port = env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
